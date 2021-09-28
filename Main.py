22f468d7594c4725de9b63d8145452d02401dca5
TheDrunkWolf21 <91485033+TheDrunkWolf21@users.noreply.github.com>
2021-09-28 15:26:33 +0300

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,17 @@
+import random
+import math
+
+def event4_1_00000001():
+        print("Hello World!")
+        
+ver = 0
+while (ver == 0):
+    print("Welcome to Dungeon Masters!")
+    print("It's a parody of  D&D")
+    print("Try to enjoy it")
+    name = input("What is your Name?\n")
+    print("Unusually")
+    print("For now your character is faceless, but later we will add personalization")
+    print("Let's start our journey")
+    ver =1
+    
